563bfc54c9037bdb3909e30f4e16295fa44bc1ef
Gilian Gonnord <63923234+GilianGonnord@users.noreply.github.com>
2022-03-20 17:21:57 +0100

    Add web-ui

---
+++ web-ui/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Hello World</Button>
+    </>
+  );
+}
+
+export default App;
